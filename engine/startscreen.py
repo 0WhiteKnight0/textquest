97689624369ab38ea0abea62e0c2e63289358bd0
0WhiteKnight0 <72785079+0WhiteKnight0@users.noreply.github.com>
2021-10-18 12:34:58 +0300

    c

---
+++ engine/startscreen.py
@@ -0,0 +1,6 @@
+from pyfiglet import Figlet
+a = Figlet(font='banner3-D')
+print (a.renderText('ARMAGEDDON'))
+print ("""Апокалиптический текстовый квест.
+Приятной игры!""")
+from engine.engine import *
--- startscreen.py
+++
@@ -1,6 +0,0 @@
-from pyfiglet import Figlet
-a = Figlet(font='slant')
-print (a.renderText('ARMAGEDDON'))
-print ("""Апокалиптический текстовый квест нового поколения.
-Приятной игры!""")
-from engine.engine import *
